71cc792855aa0126da6654ef7afd2e2a2d711667
aiprincegenius13 <ejblack369@icloud.com>
2025-03-07 11:28:17 -0700

    reated CSS
    Added CSS for Navbar

---
+++ navbar.jsx
@@ -0,0 +1,30 @@
+//Dynamic NavBar
+
+const Navbar = () => {
+    return(
+
+<div>
+<nav className="navbar">
+            <ul>
+            <li><a href="/login">Login</a></li>
+            <li><a href="/posts">Posts</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/createPost">Create Posts</a></li>
+
+            </ul>
+ </nav>
+        <div>
+            <h1>Welcome to TBD</h1>
+
+            <p>Where nothing evr stays the same, and always changes</p>
+        </div>
+
+
+    )
+
+
+</div>
+
+    )}
+
+    export default Navbar
